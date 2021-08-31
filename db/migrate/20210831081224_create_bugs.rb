551b4d1423b9e66b36b22672748c608d8282963b
ahmadejazmalik <ahmad.ejaz@engin.tech>
2021-08-31 14:37:33 +0500

    initial commit

---
+++ db/migrate/20210831081224_create_bugs.rb
@@ -0,0 +1,14 @@
+class CreateBugs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bugs do |t|
+      t.string :title
+      t.string :dedline
+      t.string :string
+      t.string :description
+      t.string :type
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
