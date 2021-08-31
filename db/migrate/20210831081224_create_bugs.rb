1c70219876d95c9d06d566c11e61a2001d489a68
ahmadejazmalik <ahmad.ejaz@engin.tech>
2021-08-31 20:11:04 +0500

    intial commit

--- db/migrate/20210831081224_create_bugs.rb
+++ db/migrate/20210831081224_create_bugs.rb
@@ -2,12 +2,11 @@ class CreateBugs < ActiveRecord::Migration[6.1]
   def change
     create_table :bugs do |t|
       t.string :title
-      t.string :dedline
+      t.string :deadline
       t.string :string
       t.string :description
       t.string :type
       t.string :status
-
       t.timestamps
     end
   end
--- db/migrate/20210831081552_create_projects.rb
+++ db/migrate/20210831081552_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
-
+      t.name
       t.timestamps
     end
   end
